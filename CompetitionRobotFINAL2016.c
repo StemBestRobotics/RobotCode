d258b512daf691a79335619ad99cd48228028f83
WaltWe <walter.weiffenbach@stemhigh.org>
2016-10-01 13:24:54 -0600

    Modified arcade drive to fit a driver request. Arcade now only goes forward and backwards outside of a certain range. Point turns made easier.

--- CompetitionRobotFINAL2016.c
+++ CompetitionRobotFINAL2016.c
@@ -37,19 +37,21 @@ void armMotorControl(){
 void arcadeDrive(){
 	int joy_x;            // will hold the X value of the analog stick (choices below)
 	int joy_y;            // will hold the Y value of the analog stick (choices below)
-	int threshold = 10;		//Threshhold in which the joystick will not move, to compensate for an imperfect joystick.
+	int threshold = 20;		//Threshhold in which the joystick will not move, to compensate for an imperfect joystick.
 
 	joy_x = vexRT[Ch4];   // This is the RIGHT analog stick.  For LEFT, change 'Ch1' to 'Ch4'.
 	joy_y = vexRT[Ch3];   // This is the RIGHT analog stick.  For LEFT, change 'Ch2' to 'Ch3'.
 
-	// Forward, and swing turns: (both abs(X) and abs(Y) are above the threshold, and Y is POSITIVE)
-	if(((abs(joy_x) > threshold) || (abs(joy_y) > threshold)) && (joy_y > 0))
+	// Forward, and swing turns: (both abs(X) and abs(Y) are above the threshold, and Y is POSITIVE and above 40)
+	//The y value being above 40 is present to mitigate a driver concern.
+	if(((abs(joy_x) > threshold) || (abs(joy_y) > threshold)) && (joy_y > 40))
 	{
 		motor[leftMotor]  = (joy_y + joy_x)/2;
 		motor[rightMotor] = (joy_y - joy_x)/2;
 	}
-	// Backwards and swing turns: (both abs(X) and abs(Y) are above the threshold, and Y is NEGATIVE)
-	else if(((abs(joy_x) > threshold) || (abs(joy_y) > threshold)) && (joy_y < 0))
+	// Backwards and swing turns: (both abs(X) and abs(Y) are above the threshold, and Y is NEGATIVE and below -40)
+	//The ya value being below -40 is present to mitigate a driver concern.
+	else if(((abs(joy_x) > threshold) || (abs(joy_y) > threshold)) && (joy_y < -40))
 	{
 		motor[leftMotor]  = (joy_y - joy_x)/2;
 		motor[rightMotor] = (joy_y + joy_x)/2;
