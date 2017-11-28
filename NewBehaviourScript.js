d71d68b4d76b2a1a1132f6d1f1326ca31b0900d6
BRH0208 <bryce@hawken.net>
2017-11-27 19:52:21 -0700

    This is all of the work I had completed

---
+++ NewBehaviourScript.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	
+	
+	transform.Rotate(Vector3.right * Time.deltaTime * 30);	
+	transform.Rotate(Vector3.right * Time.deltaTime * 30);	
+}
